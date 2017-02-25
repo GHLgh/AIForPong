0053146a00bcc4df60672a7feddc1c895e9cd8f6
Guanheng Luo <gluo2@illinois.edu>
2017-02-25 13:15:18 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     '''
     Runner code to start the training and game play.
     '''
-    simulator = Simulator( import_q_table = 'detail_q_table.cfg')
+    simulator = Simulator( import_q_table = 'q_table.cfg')
     score = simulator.play_game(False, True)
     print(score)
